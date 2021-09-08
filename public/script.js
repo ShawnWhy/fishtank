2d5a2b2f434a6cbecc7996ab363cfb60cadefd63
Shawn Yu <shawnyudesign@gmail.com>
2021-09-08 07:41:01 -0400

    craft done

--- public/script.js
+++ public/script.js
@@ -1,199 +1,222 @@
 
 
-window.addEventListener("DOMContentLoaded",function(){
-    var canvas= document.getElementById("canvas")
-    var engine = new BABYLON.Engine(canvas,true);
+var prevX=0;
+var prevY=0;
+var trottle="off "
 
-    var createScene = function(){
-        var scene = new BABYLON.Scene(engine);
-        scene.clearColor = new BABYLON.Color3(0,1,1);
-        // var camera = new BABYLON.FreeCamera("camera1", 
-        // new BABYLON.Vector3(6,10,-20),scene);
-        // camera.setTarget(BABYLON.Vector3.Zero());
-        // camera.attachControl(canvas,true);
-        // camera.keysUp.push(87);
-        // camera.keysDown.push(83);
-        // camera.keysLeft.push(65);
-        // camera.keysRight.push(68);
-        var camera = new BABYLON.UniversalCamera("UniversalCamera",
-        
-        BABYLON.Vector3(0,0,0),scene);
-        camera.position = new BABYLON.Vector3(0, 10, 40);
-        // camera.rotation.x -= Math.PI/5;
-        camera.rotation.y += Math.PI;
-        // camera.rotation.y -= Math.PI/5;
+function splash(e){
+    var number = Math.floor(Math.random()*4+6)
+    var half = number/2
+    for(i=0;i<number;i++){
+        var splash=$("<div>");
+        if(i<half||i==half){
+        splash.cdd("left", e.clientX+(i*10)+"px")
+        splash.addClass("splash");
 
- 
-        camera.attachControl(canvas,true);
+    }
+}
+function fly(e){
+    $('.creature').css("transform", "rotate(90deg)")
+    var tentacles = $(".tentacle");
+    var rootposition=$(".tentacleroot").offset()
+    baseY= rootposition.top
+    baseX= rootposition.left
+    for(let i=0;i<tentacles.length; i++){
+      
+        setTimeout(() => {
+            var color;
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var j = 0; j < 6; j++) {
+            color += letters[Math.floor(Math.random() * 16)];}
+            var classes = $(tentacles[i]).attr("class");
+            if(!classes.includes("wave")){
+                $(tentacles[i]).addClass("wave")
 
-        engine.enableOfflineSupport = false;
-        BABYLON.SceneLoader.ImportMesh("","","LORDROBOT.gltf",scene,
-        
-        function(newMeshes){
-            
-            $(".loading").addClass("invisibleP")
-            // scene.stopAllAnimations()
-            scene.animationGroups.forEach(animation=>{
-                animation.pause()})
-            
-            
-
-            $('.box').mousemove(event=>{
-                // event.stopPropagation();
-                // event.preventDefault();
-                 scene.animationGroups.forEach(animation=>{
-                                animation.play(true)})
-                              
-            })
-            $('.box').mousemove(event=>{
-                clearTimeout(stopanimation);
-                var stopanimation = setTimeout(
-                    function(){
-                        scene.animationGroups.forEach(animation=>{
-                            animation.pause()})
-                    }, 500);
-
-            })
-
-            $('.box').mouseleave(event=>{
-                scene.animationGroups.forEach(animation=>{
-                                animation.pause()})
+            }
 
 
-            })
+            $(tentacles[i]).css("top",baseY+10*i+"px")
+            $(tentacles[i]).css("left",baseX+20*i+"px")
+            $(tentacles[i]).css("background-color",color)
 
+        }, i*150);
+
+    }
+    var tentacles2 = $(".tentacle2");
+    var rootposition2=$(".tentacleroot4").offset()
+    baseY2= rootposition2.top
+    baseX2= rootposition2.left
+    for(let i=0;i<tentacles2.length; i++){
        
-         
+        setTimeout(() => {
+            var color2;
+            var letters2 = '0123456789ABCDEF';
+            var color2 = '#';
+            for (var j = 0; j < 6; j++) {
+            color2 += letters2[Math.floor(Math.random() * 16)];}
+            var classes = $(tentacles2[i]).attr("class");
+            if(!classes.includes("wave")){
+                $(tentacles2[i]).addClass("wave")
+
+            }
+            
+            $(tentacles2[i]).css("top",baseY2+10*i+"px")
+            $(tentacles2[i]).css("left",baseX2-20*i+"px")
+            $(tentacles2[i]).css("background-color",color2)
+
+        }, i*150);
+
+    }
+    var tentacles3 = $(".tentacle3");
+    var rootposition3=$(".tentacleroot3").offset()
+    baseY3= rootposition3.top
+    baseX3= rootposition3.left
+     for(let i=0;i<tentacles3.length; i++){
+
+        setTimeout(() => {
+            var classes = $(tentacles3[i]).attr("class");
+            if(!classes.includes("wave2")){
+                $(tentacles3[i]).addClass("wave2")
+
+            }
+            
+            $(tentacles3[i]).css("top",baseY3+10*i+"px")
+            $(tentacles3[i]).css("left",baseX3+"px")
+
+        }, i*150);
+
+    }
+    
+
+}
+function movetentacle(e, theta){
+    var tentacles = $(".tentacle");
+    var rootposition=$(".tentacleroot").offset()
+    baseY= rootposition.top
+    baseX= rootposition.left
+    for(let i=0;i<tentacles.length; i++){
+        setTimeout(() => {
+                     $(tentacles[i]).css("top",baseY+"px")
+            $(tentacles[i]).css("left",baseX+"px") 
+            $(tentacles[i]).css("transform", "rotate("+ theta+"deg)translate("+20*i+"px, "+5*i+"px" )
+
+        }, i*50);
+
+    }
+    var tentacles2 = $(".tentacle2");
+    var rootposition2=$(".tentacleroot2").offset()
+    baseY2= rootposition2.top
+    baseX2= rootposition2.left
+    for(let i=0;i<tentacles2.length; i++){
+        setTimeout(() => {
+            $(tentacles2[i]).css("top",baseY2+"px")
+            $(tentacles2[i]).css("left",baseX2+"px") 
+            $(tentacles2[i]).css("transform", "rotate("+ theta+"deg)translate("+20*i+"px, "+5*i+"px" )
+
+        }, i*30);
+
+    }
+    var tentacles3 = $(".tentacle3");
+    var rootposition3=$(".tentacleroot3").offset()
+    baseY3= rootposition3.top
+    baseX3= rootposition3.left
+    for(let i=0;i<tentacles3.length; i++){
+        setTimeout(() => {
+                     $(tentacles3[i]).css("top",baseY3+"px")
+            $(tentacles3[i]).css("left",baseX3+"px") 
+            $(tentacles3[i]).css("transform", "rotate("+ theta+"deg)translate("+20*i+"px, "+5*i+"px" )
+
+        }, i*20);
+
+    }
+}
+
+function rotateOcto(e){
+
+    var dy= e.clientY-prevY;
+    var dx=e.clientX-prevX;
+    if(dy!==0&&dx!==0){
+    var theta = Math.atan2(dy, dx);
+    theta *=180/Math.PI;
+    $(".creature").css("transform", "rotate("+ theta+"deg)")
+    movetentacle(e,theta);
+
+    }
+}
 
 
 
-        //  foodText.actionManager = new BABYLON.ActionManager(scene);
-        //  foodText.actionManager.registerAction(
-        //      new BABYLON.ExecuteCodeAction(
-        //          {
-        //          trigger: BABYLON.ActionManager.OnPickTrigger,
-        //          parameter:""
-        //          },
-        //          function(){
-        //                   scene.animationGroups.forEach(animation=>{
-        //              animation.play(false);
-        //          })
-                   
-                     
-        //          }
+$(window).mousemove(function (e) { 
+    var waterline=$(".waves").offset()
+    var waterline=waterline.top;
+    if(throttle="off"){
+    $(".creature").css("top",e.clientY)
+    $(".creature").css("left",e.clientX)
+    if(e.clientY>waterline){
 
-        //      ))
+     var tentacles=$(".tentacle");
+     for(i=0;i<tentacles.length;i++){
+         $(tentacles[i]).removeClass("wave");
+        //  $(tentacles[i]).css("background-color", "rgb(24, 83, 64)");
 
-        })
-        var light = new BABYLON.PointLight("pointLight",new BABYLON.Vector3(0,50,60),scene);
+     }
 
-        // light.parent = camera2;
-        light.intensity=5000;
-        light.range=100;
+     var tentacles2=$(".tentacle2");
+     for(i=0;i<tentacles2.length;i++){
+         $(tentacles2[i]).removeClass("wave");
+        //  $(tentacles2[i]).css("background-color", "rgb(24, 83, 64)");
 
-        var light2 = new BABYLON.PointLight("pointLight2",new BABYLON.Vector3(-50,0,-50),scene);
+     }
+     var tentacles3=$(".tentacle3");
+     for(i=0;i<tentacles3.length;i++){
+         $(tentacles3[i]).removeClass("wave2");
 
-        // light.parent = camera2;
-        light2.intensity=500;
-        light2.range=100;    
-        light2.diffuse = new BABYLON.Color3(.2, .3, 1);
-	    light2.specular = new BABYLON.Color3(.2, .3, 1);
+     }
+    setTimeout(() => {
+        for(i=0;i<tentacles3.length;i++){
+            $(tentacles3[i]).css("background-color", "rgb(24, 83, 64)");  
 
+        }
+        for(i=0;i<tentacles2.length;i++){
+            $(tentacles2[i]).css("background-color", "rgb(24, 83, 64)");  
 
+        }
+        for(i=0;i<tentacles.length;i++){
+            $(tentacles[i]).css("background-color", "rgb(24, 83, 64)");  
 
+        }
+    }, 50);
+    
+    if(prevX!==0&&prevY!==0){
+        
 
+        setTimeout(() => {
 
-       return scene;
+             rotateOcto(e)
+            
+        }, 200);
+    }
+    prevX=e.clientX;
+    prevY=e.clientY;
+    trottle="on"}
+
+    else{
+        fly()
+
+   prevX=e.clientX;
+   prevY=e.clientY;
+   trottle="on"
     }
 
- var scene= createScene();
- engine.runRenderLoop(function(){     
-     scene.render();
-
- });
-
-})
-
-function changecolor(event){
-    $(event.target).html("");
-    let colorsquare = $("<div>");
-    colorsquare.addClass("colorsquare")
-    let red = Math.random()*225;
-    let green = Math.random()*225;
-    let blue = Math.random()*225;
-    let color = "rgb("+red+","+green+","+ blue+")"
-    colorsquare.css("background-color", color);
-    $(event.target).append(colorsquare)
-
-}
-function changecolor2(event){
-    // $(event.target).parent().html("");
-    let colorsquare = $("<div>");
-    colorsquare.addClass("colorsquare")
-    let red = Math.random()*225;
-    let green = Math.random()*225;
-    let blue = Math.random()*225;
-    let color = "rgb("+red+","+green+","+ blue+")"
-    colorsquare.css("background-color", color);
-    $(event.target).parent().append(colorsquare)
-
-}
-
-
-$(".square").mouseover(event=>{
-    event.stopPropagation();
-    event.preventDefault();
-    changecolor(event);
-
-})
-
-$(".colorsquare").mouseover(event=>{
-    event.stopPropagation();
-    event.preventDefault();
-    changecolor2(event);
-
-})
-
-function tilt(event){
-    maxdegree= 45;
-    var coord = $(".boxcontainer").offset()
-    var topmin = coord.top;
-    var leftmin = coord.left;
-    var mousetop = event.clientY;
-    var mouseleft = event.clientX;
-    var positionOnDivX = mouseleft-leftmin;
-    var positionOnDivY = mousetop - topmin;
-    var positionXPercent = positionOnDivX/300;
-    var positionYPercent = positionOnDivY/300;
-    console.log(positionXPercent)
-    console.log(positionYPercent)
-    // $(".box").css("transform","rotate3d(-"+positionXPercent+","+positionYPercent+",0,35deg)")
-    $(".box").css("transform","rotateX("+(positionYPercent*15-30)+"deg)rotateY("+(positionXPercent*25)+"deg)")
-
-
-
-    // console.log(mousetop)
-}
-
-$(".boxcontainer").mousemove(event=>{
-    event.preventDefault();
-    event.stopPropagation();
-    tilt(event);
     
-})
-
-$('.square').click(event=>{
-    
-    $(event.target).html("")
-    $(event.target).closest(".square").html("")
-  
-})
-
-// $(".boxcontainer").mouseout(event=>{
-//     event.preventDefault();
-//     event.stopPropagation();
-//     $(".box").css("transform","translateZ(200px)rotate3d(-1,0, 0,10deg)")
+    setTimeout(() => {
+        throttle="off"
+        
+    }, 200);}
+    // values: e.clientX, e.clientY, e.pageX, e.pageY
 
     
-    
-// })
+});
+
+
